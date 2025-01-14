1a26f34da5bed7b320ca7c4d014f47b57f708818
Sarathee Woomay Sumaiyah <115939225+maiyahsarah@users.noreply.github.com>
2025-01-14 19:50:12 +0800

    Delete constants_and_globals.rb

--- constants_and_globals.rb
+++
@@ -1,5 +0,0 @@
-#Constants are declared by capitalizing every letter in the variable's name, per Ruby convention. 
-#Ruby allows you to change the value assigned to a constant.
-
-#Global variables are declared by starting the variable name with the dollar sign ($).
-
